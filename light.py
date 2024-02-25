9b29d263b7a47bad138f23a89fbe25e077f8dd2f
Marius Biller <b.majus@web.de>
2024-02-25 21:39:38 +0100

    add devices with switch datapoint, add first on off switch command

--- light.py
+++ light.py
@@ -55,29 +55,39 @@ def setup_platform(
         return
 
     devices = []
-
     for item in response.json():
-        device_info = {
-            "name": item["label"],
-            "type": item["type"],
-            "function_id": item["id"],
-            "brightness": 0  # Assuming a default brightness of 0
-        }
-        devices.append(device_info)
+        # get switch_id
+        switch_id = None
+        for datapoint in item.get("datapoints", []):
+            if datapoint.get("type") == "switch":
+                switch_id = datapoint.get("id")
+                break
+        if switch_id:
+            device_info = {
+                "name": item["label"],
+                "type": item["type"],
+                "device_id": item["id"],
+                "switch_id": switch_id,
+                "token": password
+            }
+            devices.append(device_info)
 
     add_entities(AwesomeLight(light) for light in devices)
+
+
     
     
 class AwesomeLight(LightEntity):
-    """Representation of an Awesome Light."""
 
     def __init__(self, light) -> None:
         """Initialize an AwesomeLight."""
         self._light = light
         self._name = light["name"]
-        self._function_id = light["function_id"]
-        self._state = None
-        self._brightness = None
+        self._device_id = light["device_id"]
+        self._switch_id = light["switch_id"]
+        self._token = light["token"]
+        self._state = False
+        self._brightness = 0
 
     @property
     def name(self) -> str:
@@ -86,7 +96,7 @@ class AwesomeLight(LightEntity):
 
     @property
     def unique_id(self) -> str | None:
-        return self._light["function_id"]
+        return self._light["device_id"]
         
     @property
     def brightness(self):
@@ -107,19 +117,66 @@ class AwesomeLight(LightEntity):
         You can skip the brightness part if your light does not support
         brightness control.
         """
-        self._light["brightness"] = kwargs.get(ATTR_BRIGHTNESS, 255)
+        #self._light["brightness"] = kwargs.get(ATTR_BRIGHTNESS, 255)
+        self._brightness = 100
         #self._light.turn_on()
         self._state = True
+        
+        url = f'https://junghome.local/api/junghome/functions/{self._device_id}/datapoints/{self._switch_id}'
+        headers = {
+            'accept': 'application/json',
+            'token': self._token,
+            'Content-Type': 'application/json'
+        }
+        data = {
+            "data": [
+                {
+                    "key": "switch",
+                    "value": "1"
+                }
+            ]
+        }
+        
+        try:
+            response = requests.patch(url, headers=headers, json=data, verify=False)
+            response.raise_for_status()
+            print("Light turned on successfully.")
+        except requests.exceptions.RequestException as e:
+            print(f"Failed to turn on light: {e}")
 
     def turn_off(self, **kwargs: Any) -> None:
         """Instruct the light to turn off."""
+        self._brightness = 0
         #self._light.turn_off()
         self._state = False
+        
+        url = f'https://junghome.local/api/junghome/functions/{self._device_id}/datapoints/{self._switch_id}'
+        headers = {
+            'accept': 'application/json',
+            'token': self._token,
+            'Content-Type': 'application/json'
+        }
+        data = {
+            "data": [
+                {
+                    "key": "switch",
+                    "value": "0"
+                }
+            ]
+        }
+        
+        try:
+            response = requests.patch(url, headers=headers, json=data, verify=False)
+            response.raise_for_status()
+            print("Light turned on successfully.")
+        except requests.exceptions.RequestException as e:
+            print(f"Failed to turn on light: {e}")
 
     def update(self) -> None:
         """Fetch new state data for this light.
 
         This is the only method that should fetch new data for Home Assistant.
         """
-        self._brightness = self._light["brightness"]
+        self._state =  self._state
+        self._brightness = self._brightness
 
