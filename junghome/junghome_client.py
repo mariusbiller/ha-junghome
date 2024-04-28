ac87b93d900d4d498e1c349cfefcc0ef517fba99
Marius Biller <b.majus@web.de>
2024-04-28 13:12:21 +0200

    put http requests into own functions

--- junghome/junghome_client.py
+++ junghome/junghome_client.py
@@ -3,6 +3,7 @@ import requests
 
 
 
+
 def request_devices(host: str, token: str):
     """
     Requests a list of devices from the api-junghome using the specified host and token.
@@ -15,24 +16,87 @@ def request_devices(host: str, token: str):
     list or None: A list of device dictionaries if the request is successful, None otherwise.
     """
     
+    # create url
+    url = 'https://' + host + '/api/junghome/functions/'
+    
+    # Use the generic HTTP GET request function
+    devices = http_get_request(url, token)
+    
+    if devices is None:
+        print("failed to get jung home devices.")
+        return None
+    
+    return devices
+
+
+
+# ==================================================================================
+# HTTP HELPER FUNCTIONS
+# ==================================================================================
+
+def http_get_request(url, token):
+    """
+    Sends an HTTP GET request to the specified URL with authorization provided by the token.
+
+    Parameters:
+        url (str):      The URL to which the GET request is sent.
+        token (str):    The authorization token included in the request headers.
+
+    Returns:
+        dict:   The JSON response from the server if the request is successful.
+        None:   Returns None if the request fails or raises an exception.
+    """
+    
     # Disabling SSL verification
     requests.packages.urllib3.disable_warnings()
     
     # create header
-    url = 'https://' + host + '/api/junghome/functions/'
     headers = {
         'accept': 'application/json',
         'token': token
     }
     
-    # request junghome devices
-    response = requests.get(url, headers=headers, verify=False)
-    if response.status_code != 200:
-        print(f"request junghome devices failed with status code: {response.status_code}")
-        if response.status_code == 401: print("invalid password or token")
+    # send request
+    try:
+        response = requests.get(url, headers=headers, verify=False)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"failed to get data: {e}")
         return None
-    
-    # return devices
-    devices = response.json()
-    return devices
 
+
+
+
+def http_patch_request(url, data, token):
+    """
+    Sends an HTTP PATCH request to the specified URL with the provided data and authorization token.
+
+    Parameters:
+        url (str):      The URL to which the PATCH request is sent.
+        data (dict):    The data to be sent in the request body, typically in JSON format.
+        token (str):    The authorization token included in the request headers.
+
+    Returns:
+        dict:   The JSON response from the server if the request is successful.
+        None:   Returns None if the request encounters an error or raises an exception.
+    """
+    
+    # Disabling SSL verification
+    requests.packages.urllib3.disable_warnings()
+    
+    # create header
+    headers = {
+        'accept': 'application/json',
+        'Content-Type': 'application/json',
+        'token': token
+    }
+    
+    # send request
+    try:
+        response = requests.patch(url, headers=headers, json=data, verify=False)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"failed to update data: {e}")
+        return None
