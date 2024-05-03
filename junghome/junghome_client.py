eb6ec238ca06937321856be2c456c17d6472937b
Marius Biller <b.majus@web.de>
2024-05-03 12:16:34 +0200

    use http request functions from junghome module

--- junghome/junghome_client.py
+++ junghome/junghome_client.py
@@ -1,102 +1,101 @@
 
 import requests
 
+class JunghomeGateway:
+
+    def request_devices(host: str, token: str):
+        """
+        Requests a list of devices from the api-junghome using the specified host and token.
+
+        Parameters:
+        host (str): hostename or ip address of the API host, excluding 'https://'.
+        token (str): The authentication token for API access.
+
+        Returns:
+        list or None: A list of device dictionaries if the request is successful, None otherwise.
+        """
+        
+        # create url
+        url = 'https://' + host + '/api/junghome/functions/'
+        
+        # Use the generic HTTP GET request function
+        devices = JunghomeGateway.http_get_request(url, token)
+        
+        if devices is None:
+            print("failed to get jung home devices.")
+            return None
+        
+        return devices
 
 
 
-def request_devices(host: str, token: str):
-    """
-    Requests a list of devices from the api-junghome using the specified host and token.
+    # ==================================================================================
+    # HTTP HELPER FUNCTIONS
+    # ==================================================================================
 
-    Parameters:
-    host (str): hostename or ip address of the API host, excluding 'https://'.
-    token (str): The authentication token for API access.
+    def http_get_request(url, token):
+        """
+        Sends an HTTP GET request to the specified URL with authorization provided by the token.
 
-    Returns:
-    list or None: A list of device dictionaries if the request is successful, None otherwise.
-    """
-    
-    # create url
-    url = 'https://' + host + '/api/junghome/functions/'
-    
-    # Use the generic HTTP GET request function
-    devices = http_get_request(url, token)
-    
-    if devices is None:
-        print("failed to get jung home devices.")
-        return None
-    
-    return devices
+        Parameters:
+            url (str):      The URL to which the GET request is sent.
+            token (str):    The authorization token included in the request headers.
 
-
-
-# ==================================================================================
-# HTTP HELPER FUNCTIONS
-# ==================================================================================
-
-def http_get_request(url, token):
-    """
-    Sends an HTTP GET request to the specified URL with authorization provided by the token.
-
-    Parameters:
-        url (str):      The URL to which the GET request is sent.
-        token (str):    The authorization token included in the request headers.
-
-    Returns:
-        dict:   The JSON response from the server if the request is successful.
-        None:   Returns None if the request fails or raises an exception.
-    """
-    
-    # Disabling SSL verification
-    requests.packages.urllib3.disable_warnings()
-    
-    # create header
-    headers = {
-        'accept': 'application/json',
-        'token': token
-    }
-    
-    # send request
-    try:
-        response = requests.get(url, headers=headers, verify=False)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        print(f"failed to get data: {e}")
-        return None
+        Returns:
+            dict:   The JSON response from the server if the request is successful.
+            None:   Returns None if the request fails or raises an exception.
+        """
+        
+        # Disabling SSL verification
+        requests.packages.urllib3.disable_warnings()
+        
+        # create header
+        headers = {
+            'accept': 'application/json',
+            'token': token
+        }
+        
+        # send request
+        try:
+            response = requests.get(url, headers=headers, verify=False)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"failed to get data: {e}")
+            return None
 
 
 
 
-def http_patch_request(url, data, token):
-    """
-    Sends an HTTP PATCH request to the specified URL with the provided data and authorization token.
+    def http_patch_request(url, token, data):
+        """
+        Sends an HTTP PATCH request to the specified URL with the provided data and authorization token.
 
-    Parameters:
-        url (str):      The URL to which the PATCH request is sent.
-        data (dict):    The data to be sent in the request body, typically in JSON format.
-        token (str):    The authorization token included in the request headers.
+        Parameters:
+            url (str):      The URL to which the PATCH request is sent.
+            data (dict):    The data to be sent in the request body, typically in JSON format.
+            token (str):    The authorization token included in the request headers.
 
-    Returns:
-        dict:   The JSON response from the server if the request is successful.
-        None:   Returns None if the request encounters an error or raises an exception.
-    """
-    
-    # Disabling SSL verification
-    requests.packages.urllib3.disable_warnings()
-    
-    # create header
-    headers = {
-        'accept': 'application/json',
-        'Content-Type': 'application/json',
-        'token': token
-    }
-    
-    # send request
-    try:
-        response = requests.patch(url, headers=headers, json=data, verify=False)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        print(f"failed to update data: {e}")
-        return None
+        Returns:
+            dict:   The JSON response from the server if the request is successful.
+            None:   Returns None if the request encounters an error or raises an exception.
+        """
+        
+        # Disabling SSL verification
+        requests.packages.urllib3.disable_warnings()
+        
+        # create header
+        headers = {
+            'accept': 'application/json',
+            'Content-Type': 'application/json',
+            'token': token
+        }
+        
+        # send request
+        try:
+            response = requests.patch(url, headers=headers, json=data, verify=False)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"failed to update data: {e}")
+            return None
--- junghome/light.py
+++ junghome/light.py
@@ -2,8 +2,9 @@
 from __future__ import annotations
 
 import logging
-import junghome_client as junghome
 import voluptuous as vol
+from .junghome_client import JunghomeGateway as junghome
+
 
 # Import the device class from the component that you want to support
 import homeassistant.helpers.config_validation as cv
@@ -97,14 +98,17 @@ class LightClass(LightEntity):
             supported_color_modes.add(ColorMode.BRIGHTNESS)
         self._attr_supported_color_modes = supported_color_modes
 
+
     @property
     def name(self) -> str:
         """Return the display name of this light."""
         return self._name
 
+
     @property
     def unique_id(self) -> str | None:
         return self._light["device_id"]
+        
         
     @property
     def brightness(self):
@@ -115,22 +119,21 @@ class LightClass(LightEntity):
         """
         return self._brightness
 
+
     @property
     def is_on(self) -> bool | None:
         """Return true if light is on."""
         return self._switch
 
+
+
     def turn_on(self, **kwargs: Any) -> None:
+        """Instruct the light to turn on."""
         #self._light.turn_on()
         self._switch = True
         self._brightness  = int(kwargs.get(ATTR_BRIGHTNESS,255))
         
         url = f'https://junghome.local/api/junghome/functions/{self._device_id}/datapoints/{self._brightness_id}'
-        headers = {
-            'accept': 'application/json',
-            'token': self._token,
-            'Content-Type': 'application/json'
-        }
         data = {
             "data": [
                 {
@@ -140,25 +143,18 @@ class LightClass(LightEntity):
             ]
         }
         
-        try:
-            response = requests.patch(url, headers=headers, json=data, verify=False)
-            response.raise_for_status()
-            print("Light turned on successfully.")
-        except requests.exceptions.RequestException as e:
-            print(f"Failed to turn on light: {e}")
+        response = junghome.http_patch_request(url, self._token, data)
+        if response is None: print("failed to turn on light.")
+
+
 
     def turn_off(self, **kwargs: Any) -> None:
         """Instruct the light to turn off."""
-        self._brightness = 0
         #self._light.turn_off()
         self._switch = False
+        self._brightness = 0
         
         url = f'https://junghome.local/api/junghome/functions/{self._device_id}/datapoints/{self._switch_id}'
-        headers = {
-            'accept': 'application/json',
-            'token': self._token,
-            'Content-Type': 'application/json'
-        }
         data = {
             "data": [
                 {
@@ -168,35 +164,32 @@ class LightClass(LightEntity):
             ]
         }
         
-        try:
-            response = requests.patch(url, headers=headers, json=data, verify=False)
-            response.raise_for_status()
-            print("Light turned on successfully.")
-        except requests.exceptions.RequestException as e:
-            print(f"Failed to turn on light: {e}")
+        response = junghome.http_patch_request(url, self._token, data)
+        if response is None: print("failed to turn off light.")
+
+
 
     def update(self) -> None:
         """Fetch new state data for this light.
-
         This is the only method that should fetch new data for Home Assistant.
         """
+        
         url = f'https://junghome.local/api/junghome/functions/{self._device_id}/datapoints/{self._switch_id}'
         headers = {
             'accept': 'application/json',
             'token': self._token
         }
         
-        try:
-            response = requests.get(url, headers=headers, verify=False)
-            response.raise_for_status()
-            data = response.json()
-            switch_value_str = data['values'][0]['value']
-            switch_value = bool(int(switch_value_str))
-            self._switch = switch_value
-        except requests.exceptions.RequestException as e:
-            print(f"Failed to get switch state: {e}")
+        response = junghome.http_get_request(url, self._token)
+        if response is None: 
+            print("failed get state of light.")
             return None
+        
 
-
+        switch_value_str = response['values'][0]['value']
+        switch_value = bool(int(switch_value_str))
+        
+        self._switch = switch_value
         self._brightness = self._brightness
+
 
