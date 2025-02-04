ce88038585c9c3d591d51ddc7a92d6670759b52f
Marius Biller <b.majus@web.de>
2025-02-04 22:58:57 +0100

    poll position state of covers

--- junghome_cover/cover.py
+++ junghome_cover/cover.py
@@ -2,18 +2,16 @@ from __future__ import annotations
 from typing import Any
 import asyncio
 import random
-from typing import Callable
-from .junghome_client import JunghomeGateway as junghome
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from .junghome_client import JunghomeGateway as junghome
 from . import HubConfigEntry
-from .const import (DOMAIN, MANUFACTURER)
+from .const import DOMAIN, MANUFACTURER
 from homeassistant.components.cover import (
     ATTR_POSITION,
     CoverEntityFeature,
     CoverEntity,
 )
-
 import logging
 _LOGGER = logging.getLogger(__name__)
 
@@ -46,18 +44,16 @@ async def async_setup_entry(
         if device["type"] not in ["Position", "PositionAndAngle"]:
             continue
         
-        # get state_id
+        # Find the state id for the cover position
         state_id = None
         for datapoint in device.get("datapoints", []):
             if datapoint.get("type") == "level":
                 state_id = datapoint.get("id")
                 break
         
-        
-        # init cover class
-        covers.append(WindowCover( device.get("id"), state_id, device.get("label"), hub))
+        # Create the cover entity
+        covers.append(WindowCover(device.get("id"), state_id, device.get("label"), hub))
     
-    # Add all entities to HA
     async_add_entities(covers)
 
 
@@ -65,46 +61,31 @@ async def async_setup_entry(
 # WINDOW COVER
 #
 class WindowCover(CoverEntity):
-    should_poll = False
-    supported_features = CoverEntityFeature.SET_POSITION | CoverEntityFeature.OPEN | CoverEntityFeature.CLOSE
+    should_poll = True
+    supported_features = (
+        CoverEntityFeature.SET_POSITION | CoverEntityFeature.OPEN | CoverEntityFeature.CLOSE
+    )
 
-    def __init__(self, device_id: str, state_id: str, name: str, hub: Hub) -> None:
-        self._device_id = device_id 
+    def __init__(self, device_id: str, state_id: str, name: str, hub: HubConfigEntry) -> None:
+        self._device_id = device_id
         self.roller_id = state_id
         self._ip = hub.ip
         self._token = hub.token
-        self._id = device_id
         self.name = name
-        self._callbacks = set()
-        self.position = 65
-        self.moving = 0  # >0 is up, <0 is down
+
+        # Initialize state variables
+        self.position = 50  # starting position (example)
+        self.moving = 0     # positive for opening, negative for closing
 
         self._attr_unique_id = f"{self.roller_id}_cover"
         self._attr_name = self.name
         
-        _LOGGER.error(f"Initialized Roller: {self.name} with ID {self._id}")
-
-    async def async_added_to_hass(self) -> None:
-        """Run when this Entity has been added to HA."""
-        # Importantly for a push integration, the module that will be getting updates
-        # needs to notify HA of changes. The device has a registercallback
-        # method, so to this we add the 'self.async_write_ha_state' method, to be
-        # called where ever there are changes.
-        self._callbacks.add(self.async_write_ha_state)
-        _LOGGER.debug(f"Callback registered for Roller {self.name}")
-
-    async def async_will_remove_from_hass(self) -> None:
-        """Entity being removed from hass."""
-        # The opposite of async_added_to_hass. Remove any registered call backs here.
-        self._callbacks.discard(self.async_write_ha_state)
-        _LOGGER.debug(f"Callback removed for Roller {self.name}")
-
-    # Information about the parent device visible in the UI.
+        _LOGGER.debug(f"Initialized cover: {self.name} with ID {self._device_id}")
     @property
     def device_info(self) -> DeviceInfo:
         return {
             "identifiers": {(DOMAIN, self.roller_id)},
-            "name": self.name,  
+            "name": self.name,
             "sw_version": "0.0.0",
             "model": "WindowCover",
             "manufacturer": MANUFACTURER,
@@ -136,7 +117,7 @@ class WindowCover(CoverEntity):
         """Return if the cover is opening or not."""
         return self.moving > 0
 
-
+    # OPEN
     async def async_open_cover(self, **kwargs: Any) -> None:
         """Open the cover."""
         self.position  = 0
@@ -145,14 +126,14 @@ class WindowCover(CoverEntity):
         url = f'https://{self._ip}/api/junghome/functions/{self._device_id}/datapoints/{self.roller_id}'
         body = {
             "data": [{
-                        "key": "level",
-                        "value": str(int(self.position))
-                    }]
+                "key": "level",
+                "value": str(int(self.position))
+            }]
         }
         response = await junghome.http_patch_request(url, self._token, body)
         if response is None: print("failed to move on cover.")
 
-
+    # CLOSE
     async def async_close_cover(self, **kwargs: Any) -> None:
         """Close the cover."""
         self.position  = 100
@@ -161,32 +142,48 @@ class WindowCover(CoverEntity):
         url = f'https://{self._ip}/api/junghome/functions/{self._device_id}/datapoints/{self.roller_id}'
         body = {
             "data": [{
-                        "key": "level",
-                        "value": str(int(self.position))
-                    }]
+                "key": "level",
+                "value": str(int(self.position))
+            }]
         }
         response = await junghome.http_patch_request(url, self._token, body)
         if response is None: print("failed to move on cover.")
 
 
+    # SET POSITION
     async def async_set_cover_position(self, **kwargs: Any) -> None:
         """Set the cover position."""
         self.position  = int(kwargs[ATTR_POSITION])
         url = f'https://{self._ip}/api/junghome/functions/{self._device_id}/datapoints/{self.roller_id}'
         body = {
             "data": [{
-                        "key": "level",
-                        "value": str(int(self.position))
-                    }]
+                "key": "level",
+                "value": str(int(self.position))
+            }]
         }
         response = await junghome.http_patch_request(url, self._token, body)
         if response is None: print("failed to move on cover.")
+        
+        
+    # UPDATE
+    async def async_update(self) -> None:
+        """
+        Fetch new state data for this cover.
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        url = f'https://{self._ip}/api/junghome/functions/{self._device_id}/datapoints/{self.roller_id}'
+        headers = {
+            'accept': 'application/json',
+            'token': self._token
+        }
+        
+        response = await junghome.http_get_request(url, self._token)
+        if response is None: 
+            print("failed get state of cover.")
+            return None
+        
+        value_str = response['values'][0]['value']
+        self.position = int(value_str)
 
 
-    async def publish_updates(self) -> None:
-        """Schedule call all registered callbacks."""
-        self.position = 30
-        _LOGGER.debug(f"Roller {self.name} updated position to {self.position}")
-        for callback in self._callbacks:
-            callback()
 
